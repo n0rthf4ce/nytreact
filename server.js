2163bb73e49b92adac37a1e1b3cf87d5b3f39550
n0rthf4ce <eric.liu85@gmail.com>
2018-07-27 12:39:34 -0700

    add necessary code

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require("./routes/apiRoutes");
+
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/nytreact";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// Define API routes here
+app.use("/api", apiRoutes);
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
